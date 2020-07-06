8597bfe6767294147f36ad7a1ea339d3e8a66fd0
Adam Pawlik <pawlik.addam@gmail.com>
2020-07-06 15:08:32 -0700

    use didSet for flipCount
    Co-Authored-By: lopienko <lopienko@users.noreply.github.com>

--- Memorygame/ViewController.swift
+++ Memorygame/ViewController.swift
@@ -22,17 +22,16 @@ class ViewController: UIViewController {
     
     @IBOutlet var cardButtons: [UIButton]!
     
-    static var count = 0
+    var flipCount = 0 {didSet { flipCountLabel.text = "Flip Count: \(flipCount)" }}
     
     func flipCard(with emoji:String, on button: UIButton){
-        ViewController.count += 1
-        flipCountLabel.text = "Flip Count: \(ViewController.count)"
+        flipCount += 1
         if button.currentTitle != emoji {
             button.backgroundColor =  colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
-            button.setTitle(emoji, for: UIControl.State.normal)
+            button.setTitle(emoji, for: UIControlState.normal)
         }else {
             button.backgroundColor =  colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
-            button.setTitle("", for: UIControl.State.normal)
+            button.setTitle("", for: UIControlState.normal)
         }
         
     }
