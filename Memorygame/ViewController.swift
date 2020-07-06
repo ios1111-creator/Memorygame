9227c239fe1fc0f270f2fcbc693dbb9cdf1fa166
Adam Pawlik <pawlik.addam@gmail.com>
2020-07-06 15:03:46 -0700

    added cards
    Co-Authored-By: lopienko <lopienko@users.noreply.github.com>

--- Memorygame/ViewController.swift
+++ Memorygame/ViewController.swift
@@ -10,16 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBAction func touchCard(_ sender: UIButton) {
+        if cardButtons.index(of: sender) != nil{
+           flipCard(with: "👻", on: sender)
+        }else {
+            print("card is not in cardButton array")
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    static var count = 0
+    
+    func flipCard(with emoji:String, on button: UIButton){
+        ViewController.count += 1
+        flipCountLabel.text = "Flip Count: \(ViewController.count)"
+        if button.currentTitle != emoji {
+            button.backgroundColor =  colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+            button.setTitle(emoji, for: UIControl.State.normal)
+        }else {
+            button.backgroundColor =  colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+            button.setTitle("", for: UIControl.State.normal)
+        }
+        
     }
-
-
+    
+   
+    
 }
-
